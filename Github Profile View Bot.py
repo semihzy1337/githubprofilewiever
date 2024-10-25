dbae95574ca2b91c2ff8ae9a671ea8395de77807
Semih <semihzy@protonmail.com>
2024-10-25 18:00:14 +0300

    Update Github Profile View Bot.py

--- Github Profile View Bot.py
+++ Github Profile View Bot.py
@@ -8,7 +8,7 @@ def send_requests(url, count):
         except requests.exceptions.RequestException as e:
             print(f"istek basarisiz {i+1}: Hata kodu: {e}")
 
-url = 'https://camo.githubusercontent.com/f5d3432c3d9c7fec2b26e2595800990abe48f429a6da2216324f537a393bf67d/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d73656d69687a7931333337'
-count = 1000000
+url = ''
+count = 
 send_requests(url, count)
     
